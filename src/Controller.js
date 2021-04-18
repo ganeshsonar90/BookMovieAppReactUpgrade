c75985dfce722dea87063adfd386003517ed3679
ganesh sonar <ganeshsonar590@gmail.com>
2021-04-18 15:58:16 +0530

    Initial commit

---
+++ src/Controller.js
@@ -0,0 +1,33 @@
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './screens/home/Home';
+import Details from './screens/details/Details';
+import BookShow from "./screens/bookshow/BookShow";
+import Confirmation from "./screens/confirmation/Confirmation";
+
+
+const Controller = () => {
+    const baseUrl = "/api/v1/";
+
+
+    return (
+
+
+        <Router>
+            <div className="main-container">
+                <Route exact path="/" render={(props) => <Home {...props} baseUrl={baseUrl}/>}/>
+
+                <Route exact path="/movie/:id" render={(props) => <Details {...props} baseUrl={baseUrl}/>}/>
+
+                <Route exact path="/bookshow/:id" render={(props) => <BookShow {...props} baseUrl={baseUrl}/>}/>
+
+
+                <Route exact path="/confirm/:id" render={(props) => <Confirmation {...props} baseUrl={baseUrl}/>}/>
+
+
+            </div>
+        </Router>
+
+    );
+
+
+};
---
+++ src/common/header/Header.js
@@ -0,0 +1,6 @@
+const Header = (props) => {
+
+};
+
+
+export default Header;
